47ef95a0cd9b221eee7b1e1967c6b170c8d8ab6c
Dominiworld <dominiworld@gmail.com>
2015-05-21 11:27:59 +0600

    Update first.c

--- first/first.c
+++ first/first.c
@@ -187,7 +187,7 @@ BigNumber Divide(BigNumber a, BigNumber b, BigNumber *mod)
 
 	for (int i = result.size; i != 0; i--)
 	{
-		unsigned long long Max = 18446744073709551615ULL, Min = 0, Mid;
+		unsigned long long Max = 18446744073709551615LLU, Min = 0, Mid;
 
 		Mid = Max;
 
@@ -617,7 +617,138 @@ BigNumber ReadBinFile(char* file)
 	}
 	return number;
 }
+
+
+char* toString(BigNumber number)
+{
+	BigNumber numcopy;
+	MemoryAllocation(&numcopy, number.size);
+	Copy(&numcopy,number);
+
+	BigNumber tmp;
+
+	unsigned long long ost;
+	unsigned long long k = 1;
+
+	if(ShortCompare(number,0)==0)
+		return "0";	
+
+	FILE* stream;
+
+	if ((stream = tmpfile())==NULL)
+	
+	{
+		printf("Temporary file wasn't created!");
+		return "";
+	}
+
+	unsigned long long size = 0;
+
+	while (ShortCompare(numcopy, 0) == 1)
+	{
+		tmp = numcopy;
+		numcopy = ShortDivide(numcopy, 10000000000000000000, &ost);
+		FreeMemory(&tmp);
+
+		char buffer[] = "0000000000000000000";
+		int k = 18;
+		while (ost > 0)
+		{
+			buffer[k--] = ost % 10 + '0';
+			ost /= 10;	
+		}
+		fprintf(stream, "%s", buffer);
+		size++;        
+	}
+
+
+	FreeMemory(&numcopy);
+
+	long long offset = -19;
+
+	fseek(stream, offset, SEEK_END);
+
+	char* str = (char*)malloc(sizeof(char)*(size+1)*19);
+
+	memset(str, '\0', (size+1)*19);
+
+	char tmps [19];	
+	fgets(tmps, 20, stream);
+	int count = 0;
+	while(tmps[count]=='0')
+		count++;
+	int s = 19-count;
+	char sss[19];
+	memset(sss, '\0', 19);
+	int i = 0;
+	while(i!=s)
+		sss[i++]=tmps[count++];
+	strcat(str,sss);
+	offset-=19;
+	fseek(stream, offset, SEEK_END);
+
+	while(offset>-(size+1)*19)
+	{
+		fgets(tmps, 20, stream);
+		strcat(str, tmps);
+		offset-=19;
+		fseek(stream, offset, SEEK_END);
+	}
+
+
+	fclose(stream);
 	
 
+	return str;
+}	
 
+BigNumber ReadFromString(char* buffer)
+{
+	int size = 0;
+	
+	BigNumber res;
+	MemoryAllocation(&res, 1);
 
+	if (buffer == NULL)
+	{
+		printf("Error by reading string!");
+		res.size = 0;
+		return res;
+	}
+	
+	if (strlen(buffer) == 0)
+	{
+		res.size = 0;
+		return res;
+	}
+	
+	BigNumber k;
+	MemoryAllocation(&k,1);
+	k.block[0] = 1;
+		
+	BigNumber tmp1;
+	BigNumber tmp2;
+
+	unsigned long long tmp = 0;
+	char ch;
+
+	for (int i = strlen(buffer)-1; i>-1; i--)
+	{
+	        ch = buffer[i];
+		if ((ch > '9') || (ch < '0'))
+		{
+			printf("Incorrect input!");
+			res.size = 0;
+			return res;
+		}
+		tmp = ch - '0';			
+		tmp1 = ShortMul(k, tmp);
+		tmp2 = Normalize(&tmp1);
+		res = Sum(res, tmp2);
+		FreeMemory(&tmp2);
+		k = ShortMul(k,10);
+	}
+	
+	FreeMemory(&k);
+	return res;
+}
